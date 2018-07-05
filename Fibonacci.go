f5c75176cb239a488e47a5214982815885ca550c
Matías Medrano Gamonal <matiasmedrano94@gmail.com>
2018-07-05 00:31:22 -0400

    Service works

--- Fibonacci.go
+++ Fibonacci.go
@@ -3,22 +3,42 @@ package main
 import (
     "fmt"
     "log"
-    "strconv"
     "net/http"
+    "os"
+    "strconv"
+    "encoding/json"
 )
 
-func Fibonacci(n int) int {
+func fibonacci(n int) int {
     if n <= 1 {
         return n
     }
-    return Fibonacci(n-1) + Fibonacci(n-2)
+    return fibonacci(n-1) + fibonacci(n-2)
+}
+
+func populateJson(n int) string{
+    mapD := map[string]int{"n": fibonacci(n), "n-1": fibonacci(n-1)}
+    mapB, _ := json.Marshal(mapD)
+    return (string(mapB))
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Fibonacci: " + strconv.Itoa(Fibonacci(8)))
+    numbers, ok := r.URL.Query()["number"]
+    if !ok || len(numbers[0]) < 1 {
+        log.Println("Url Param 'number' is missing")
+        os.Exit(2)
+    }
+    number := numbers [0]
+    log.Println("Url Param 'number' is: " + string(number))
+    iNumber, err := strconv.Atoi(number)
+    if err != nil {
+       fmt.Println(err)
+       os.Exit(2)
+    }
+    fmt.Fprintf(w, populateJson(iNumber))
 }
 
 func main() {
     http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":8081", nil))
+    log.Fatal(http.ListenAndServe(":3004", nil))
 }
